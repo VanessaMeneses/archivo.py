f1653daf01ceec930fbe4bf1f544b02af70a379e
VanessaMeneses <145416439+VanessaMeneses@users.noreply.github.com>
2023-10-06 15:28:36 -0500

    Add files via upload

---
+++ ejercicioentregable1.py
@@ -0,0 +1,7 @@
+def descripcion_datos_persona(nombre: str, edad: int, altura: float):
+    print("nombre: " + nombre)
+    print("edad: " + str(edad))
+    print("altura: " + str(altura))
+
+descripcion_datos_persona("Enzo", 29, 3.5)
+#Un programa que me diga si el numero es positivo, negativo o cero usando una funcion
