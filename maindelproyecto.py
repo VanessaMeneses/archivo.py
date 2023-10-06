ded1ab42c3962cda10b0fc01caf5059934b9282a
VanessaMeneses <145416439+VanessaMeneses@users.noreply.github.com>
2023-10-06 15:47:50 -0500

    Add files via upload

---
+++ maindelproyecto.py
@@ -0,0 +1,7 @@
+# Archivo principal del proyecto integrador 2023 cohorte 50.
+# Videojuego de texto 
+# Creamos la variable nombre para que el ususario puede regsitrarse y en seguida damos un mensaje de bienvenida.
+nombre = str(input('Ingrese el nombre de usuario: '))
+print(f'Hola {nombre} bienvenid@ a esta maravillosa experiencia!')
+
+## if __(main) == '__(main)__':
